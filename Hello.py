0670734eec85fd80171c0c7a97ec62eef26dbede
Mahmoud Tawfik <60586286+mmtawfik@users.noreply.github.com>
2023-12-24 14:38:51 +0200

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -14,14 +14,14 @@ def hash_password(password):
     key = hashlib.pbkdf2_hmac('sha256', password.encode('utf-8'), salt, 100000)
     return (salt + key).hex()
 
-hashed_password = hash_password('engyshahbander')
+hashed_password = hash_password('drengyoxyhealth')
 
 username = st.text_input("Username", "")
 password = st.text_input("Password", type='password')
 
 
 if st.button("Login"):
-    if username == 'Engy' and password == 'engyshahbander':
+    if username == 'drengyoxyhealth' and password == hashed_password:
             # Run your app's main functionality here
             st.write("Welcome, you are logged in!")
             

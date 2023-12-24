f3984887c04b116aad28420bf0d2e58c9e2be271
Mahmoud Tawfik <60586286+mmtawfik@users.noreply.github.com>
2023-12-24 13:50:49 +0200

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -7,75 +7,78 @@ import tempfile
 from pathlib import Path
 import os
 from fpdf import FPDF
-import streamlit_authenticator
-from streamlit_authenticator import Authenticator
-auth = Authenticator()
-
-auth.login(
-    st,
-    title="Please Login",
-    email="engy.shahbander",
-    password="engyshahbander.50495049",
-    admin=False,
-    error_message="Your username or password is incorrect. Please try again."
-)
+import hashlib
 
 
-    
+def hash_password(password):
+    salt = os.urandom(32)
+    key = hashlib.pbkdf2_hmac('sha256', password.encode('utf-8'), salt, 100000)
+    return (salt + key).hex()
+
+# Hash the password using the above function
+hashed_password = hash_password('engyshahbander.50495049')
+
+username = st.text_input("Username", "")
+password = st.text_input("Password", type='password')
+
+if st.button("Login"):
+    if username == 'your_username_here' and hash_password(password) == hashed_password:
+        st.session_state['user_logged_in'] = True
+    else:
+        st.error('Invalid username or password')
+
+if user_logged_in:
+    # Run your app's main functionality here
+    st.write("Welcome, you are logged in!")
+
+def create_prescription(patient_name, date, day, birthday, prescription):
+    image_path = 'prescription.png' # Replace this with the actual path to your prescription.png file
+    image = Image.open(image_path)
+    image = image.convert("RGB") # Convert image to RGB mode
+    draw = ImageDraw.Draw(image)
+    font = ImageFont.truetype("Arial.ttf", 28)
 
 
-if auth.is_authenticated:
+    draw.text((220, 395), patient_name, (0, 0, 0), font=font)
+    draw.text((745, 395), date, (0, 0, 0), font=font)
+    draw.text((300, 455), birthday, (0, 0, 0), font=font)
+    draw.text((120, 520), prescription, (0, 0, 0), font=font)
 
-    def create_prescription(patient_name, date, day, birthday, prescription):
-        image_path = 'prescription.png' # Replace this with the actual path to your prescription.png file
-        image = Image.open(image_path)
-        image = image.convert("RGB") # Convert image to RGB mode
-        draw = ImageDraw.Draw(image)
-        font = ImageFont.truetype("Arial.ttf", 28)
-    
-    
-        draw.text((220, 395), patient_name, (0, 0, 0), font=font)
-        draw.text((745, 395), date, (0, 0, 0), font=font)
-        draw.text((300, 455), birthday, (0, 0, 0), font=font)
-        draw.text((120, 520), prescription, (0, 0, 0), font=font)
-    
-        image.save('prescription.jpg', format='JPEG') # Save as JPEG format
-        st.image(image, caption='Prescription', use_column_width=True)
-    
-        # Convert the image to a PDF file
-        create_pdf('prescription.jpg', patient_name + '.pdf')
-    
-    def display_image(image_data):
-        image = Image.open(BytesIO(image_data))
-        st.image(image, caption='Input Image', use_column_width=True)
-    
-    def create_pdf(image_file, pdf_file):
-        pdf = FPDF()
-        pdf.add_page()
-        pdf.set_font("Arial", size=12)
-    
-        pdf.image(image_file, x=0, y=0, w=210)
-        pdf.output(pdf_file)
-    
-    def create_app():
-        st.title('Create Prescription App')
-        st.markdown("This app helps you create a prescription by filling in the details.")
-    
-        patient_name = st.text_input("Patient Name", "")
-        prescription_date = datetime.date.today().strftime("%d-%m-%Y")
-        day = datetime.datetime.today().strftime("%A")
-        birthday = st.date_input("Patient's Birthday", datetime.date(1980, 7, 6))
-        prescription = st.text_area("Prescription")
-    
-        submit = st.button("Create Prescription")
-    
-        if submit:
-            create_prescription(patient_name, prescription_date, day, str(birthday), prescription)
-            pdf_file = patient_name + '.pdf'
-            if os.path.exists(pdf_file):
-                st.download_button(label="Download Prescription", data=open(pdf_file, 'rb'), file_name=pdf_file, mime='application/pdf')
-            
-    if __name__ == '__main__':
-        create_app()
-else:
-    st.markdown("Please login to use the app.")
+    image.save('prescription.jpg', format='JPEG') # Save as JPEG format
+    st.image(image, caption='Prescription', use_column_width=True)
+
+    # Convert the image to a PDF file
+    create_pdf('prescription.jpg', patient_name + '.pdf')
+
+def display_image(image_data):
+    image = Image.open(BytesIO(image_data))
+    st.image(image, caption='Input Image', use_column_width=True)
+
+def create_pdf(image_file, pdf_file):
+    pdf = FPDF()
+    pdf.add_page()
+    pdf.set_font("Arial", size=12)
+
+    pdf.image(image_file, x=0, y=0, w=210)
+    pdf.output(pdf_file)
+
+def create_app():
+    st.title('Create Prescription App')
+    st.markdown("This app helps you create a prescription by filling in the details.")
+
+    patient_name = st.text_input("Patient Name", "")
+    prescription_date = datetime.date.today().strftime("%d-%m-%Y")
+    day = datetime.datetime.today().strftime("%A")
+    birthday = st.date_input("Patient's Birthday", datetime.date(1980, 7, 6))
+    prescription = st.text_area("Prescription")
+
+    submit = st.button("Create Prescription")
+
+    if submit:
+        create_prescription(patient_name, prescription_date, day, str(birthday), prescription)
+        pdf_file = patient_name + '.pdf'
+        if os.path.exists(pdf_file):
+            st.download_button(label="Download Prescription", data=open(pdf_file, 'rb'), file_name=pdf_file, mime='application/pdf')
+        
+if __name__ == '__main__':
+    create_app()

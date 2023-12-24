fbad5188d8635b6a61d014cad7f2e4774cda84e5
Mahmoud Tawfik <60586286+mmtawfik@users.noreply.github.com>
2023-12-24 14:13:46 +0200

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -16,13 +16,15 @@ hashed_password = hash_password('Engy')
 username = st.text_input("Username", "")
 password = st.text_input("Password", type='password')
 
+
+user_logged_in = False
+
 if st.button("Login"):
     if username == 'Engy' and hash_password(password) == hashed_password:
         st.session_state['user_logged_in'] = True
     else:
         st.error('Invalid username or password')
 
-user_logged_in = False
 
 if user_logged_in:
     # Run your app's main functionality here

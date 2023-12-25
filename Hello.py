5d64716f168e6843c3718b5722eb8a686cccc958
Mahmoud Tawfik <60586286+mmtawfik@users.noreply.github.com>
2023-12-25 15:20:00 +0200

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -72,7 +72,7 @@ def create_app():
     patient_name = st.text_input("Patient Name", "")
     prescription_date = date.today().strftime("%d-%m-%Y")
     day = date.today().strftime("%A")
-    birthday = st.date_input("Patient's Birthday", datetime.date(1980, 7, 6).date())
+    birthday = st.date_input("Patient's Birthday", datetime.date(1980, 7, 6))
     prescription = st.text_area("Prescription")
 
 
